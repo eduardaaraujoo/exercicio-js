7c03e7d4adf62f722f1b7c2a8cba068a2c4bedd4
Maria Eduarda de Araujo Santos <maria.easantos@PMJG.LAN>
2023-12-19 13:27:21 -0300

    first commit

---
+++ exerc-arrays/index.js
@@ -0,0 +1,10 @@
+const alunos = ['Eduarda', 'Barbara', 'Juliana', 'Thaiane'];
+
+//console.log(alunos[2]);
+//alunos.unshift('Maria');
+//const removido = alunos.pop('Thaiane');
+//const removido = alunos.shift();
+///console.log(alunos);
+//console.log(removido);
+console.log(alunos);
+
---
+++ exerc-function/index.js
@@ -0,0 +1,17 @@
+function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+}
+
+
+console.log(soma(7,8));
+console.log(soma(3, 9));
+console.log(soma(10,20));
+
+const raiz = (n) => {
+    return n ** 0.5;
+}
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
