d46c3280c1d9514a77dae8b5da1b68ba08ed5304
Maria Eduarda de Araujo Santos <maria.easantos@PMJG.LAN>
2023-12-27 14:01:25 -0300

    Add examples in index.js

--- exerc-primitivos/index.js
+++ exerc-primitivos/index.js
@@ -1,3 +1,15 @@
-let a = ['maçã', 'pera', 'uva'];
-let b = a;
-console.log(a, b);
+//let a = ['maçã', 'pera', 'uva'];
+//let b = a;
+//console.log(a, b);
+
+
+const a = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+};
+
+const b = a;
+
+b.nome = 'João'
+console.log(a);
+console.log(b);
