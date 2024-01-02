04f668d154d4119620255a8a85f55a295a028b41
Maria Eduarda de Araujo Santos <dudsaraujo16@gmail.com>
2024-01-02 16:56:38 -0300

    Added else if

--- exerc-if/index.js
+++ exerc-if/index.js
@@ -1,5 +1,9 @@
-const hora = 16;
+const hora = 22;
 
-if (hora < 12) {
+if (hora >= 0 && hora <= 11) {
     console.log('Bom dia!')
-} 
+} else if (hora >=  12 && hora <= 17) {
+    console.log('Boa tarde!')
+} else if (hora >= 18 && hora <= 23) {
+    console.log('Boa noite!')
+}
