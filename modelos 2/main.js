22c5e159869d993599e3c15cb66b012f13794f48
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-03-19 12:29:40 -0300

    Change the class container

--- modelos 2/main.js
+++ modelos 2/main.js
@@ -1,40 +1,46 @@
-function getTimeFromSeconds(seconds){
-    const data = new Date(seconds * 1000);
-    return data.toLocaleTimeString('pt-Br', {
-        hour12: false,
-        timeZone: 'UTC'
+function clock(){
+    function getTimeFromSeconds(seconds){
+        const data = new Date(seconds * 1000);
+        return data.toLocaleTimeString('pt-Br', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+    
+    
+    const clock = document.querySelector('.clock');
+    let seconds = 0; 
+    let timer;
+    
+    function startClock(){
+         timer = setInterval(function(){
+            seconds++;
+            clock.innerHTML = getTimeFromSeconds(seconds);
+        }, 1000);
+    }
+    
+    
+    document.addEventListener('click', function(e){
+        const el = e.target;
+    
+        if(el.classList.contains('reset')){
+            clearInterval(timer);
+            clock.innerHTML = '00:00:00';
+            clock.classList.remove('paused');
+            seconds = 0;
+        }
+    
+        if(el.classList.contains('start')){
+            clock.classList.remove('paused');
+            clearInterval(timer);
+            startClock();
+        }
+    
+        if(el.classList.contains('pause')){
+            clearInterval(timer);
+            clock.classList.add('paused')
+        }
     });
+    
 }
-
-
-const clock = document.querySelector('.clock');
-const start = document.querySelector('.start');
-const pause = document.querySelector('.pause');
-const reset = document.querySelector('.reset');
-let seconds = 0; 
-let timer;
-
-function startClock(){
-     timer = setInterval(function(){
-        seconds++;
-        clock.innerHTML = getTimeFromSeconds(seconds);
-    }, 1000);
-}
-
-
-start.addEventListener('click', function(event){
-    clock.classList.remove('paused')
-    clearInterval(timer);
-    startClock();
-});
-
-pause.addEventListener('click', function(event){
-    clearInterval(timer);
-    clock.classList.add('paused')
-});
-
-reset.addEventListener('click', function(event){
-    clearInterval(timer);
-    clock.innerHTML = '00:00:00';
-    seconds = 0;
-});
+clock();
