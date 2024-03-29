f47da5e370702f42b2865ec2a4dec3dfe8be687b
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-03-29 15:07:46 -0300

    Create the folder declarar function

---
+++ declarar-function/index.js
@@ -0,0 +1,12 @@
+//Declaração de função (Function hoisting)
+function falaOi() {
+    console.log('Oie');
+}
+falaOi();
+
+// First-class objects (objetos de primeira classe)
+
+ const jogaDado = function() {
+    console.log('Joguei o dado');
+ };
+ jogaDado();
