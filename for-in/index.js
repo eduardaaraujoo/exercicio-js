1c11d3f2b8df14cbb50e6e6046cd10d2c0be6adc
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-03-29 14:32:57 -0300

    Create the folder for-of

--- for-in/index.js
+++ for-in/index.js
@@ -1,12 +1,29 @@
-
-//                0         1         2
+//For in ler os indices ou chaves do objeto
+//  ARRAYS        0         1         2
 const frutas = ['Maçã', 'Laranja', 'Manga'];
 
    // for (let i = 0; i < frutas.length; i++) {
      //   console.log(frutas[i]);
    // }
 
-   for (let i in frutas) {
-    console.log(frutas [i]);
-   }
+
+ //  for (let i in frutas) {
+  //  console.log(frutas [i]);
+  // }
+
+//OBJETO
+const pessoa = {
+    nome: 'Eduarda',
+    sobrenome: 'Araújo',
+    idade: 26
+};
+
+for (let chave in pessoa){
+    console.log(chave);
+  // console.log(pessoa[chave])
+
+    //caso queira acessar apenas uma chave
+    //console.log(pessoa.idade);
+}
+
 
