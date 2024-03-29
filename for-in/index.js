29e2d2ff627a386b1615340fb367ffd7ee0fae2f
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-03-29 14:12:11 -0300

    Create the folder for-in

---
+++ for-in/index.js
@@ -0,0 +1,12 @@
+
+//                0         1         2
+const frutas = ['Maçã', 'Laranja', 'Manga'];
+
+   // for (let i = 0; i < frutas.length; i++) {
+     //   console.log(frutas[i]);
+   // }
+
+   for (let i in frutas) {
+    console.log(i);
+   }
+
