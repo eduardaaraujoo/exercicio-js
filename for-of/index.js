19e3c4a2e9dbfa93c38bfce71cdc946ff6541fdb
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-03-29 14:43:57 -0300

    Mofified for-of/index.js

---
+++ for-of/index.js
@@ -0,0 +1,11 @@
+const nomes = 'Eduarda Araújo';
+
+
+//for (let i in nome){
+   // console.log(nome[i]); 
+    //vai acessar cada índice de nome
+//}
+
+for (let valor of nomes){
+    console.log(valor);
+}
