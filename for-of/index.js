7e044a65f063a45a53b829f05f388eaa9e214bf9
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-03-29 14:54:32 -0300

    Modified index.js

--- for-of/index.js
+++ for-of/index.js
@@ -1,11 +1,18 @@
-const nomes = 'Eduarda Araújo';
+const nomes = ['Eduarda', 'Araújo'];
 
-
+//FOR IN 
 //for (let i in nome){
    // console.log(nome[i]); 
     //vai acessar cada índice de nome
 //}
 
+//FOR OF 
 for (let valor of nomes){
     console.log(valor);
 }
+
+console.log('###')
+//FOR EACH
+nomes.forEach(function(valor, indice, array){
+    console.log(valor, indice, array)
+});
