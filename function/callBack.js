79892aaab2d367bea2ca36c307e31f378a093192
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-04-09 10:10:31 -0300

    Modified the callBack.js

--- function/callBack.js
+++ function/callBack.js
@@ -6,23 +6,34 @@ function rand(min= 1000, max= 3000){
 function f1(callback){
     setTimeout(function() { //permite que você execute um trecho de código (neste caso, uma função anônima) após um determinado intervalo de tempo.
         console.log('f1');
+        if (callback) callback();
     }, rand());  
 }
 
 function f2(callback){
     setTimeout(function() { 
         console.log('f2');
+        if (callback) callback();
     }, rand()); 
 }
 
 function f3(callback){
     setTimeout(function() { 
         console.log('f3');
-    }, RadioNodeList
-     ), rand(); 
+        if (callback) callback();
+    }, rand()); 
 }
 
-f1();
-f2();
-f3();
-console.log('Olá Mundo');
+f1(f1Callback);
+
+function f1Callback(){
+    f2(f2Callback);
+}
+
+function f2Callback(){
+    f3(f3Callback);
+}
+
+function f3Callback(){
+    console.log('Hello World!');
+}
