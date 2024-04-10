52912a06380180af60b5738e6e6efe45cde14718
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-04-10 12:05:08 -0300

    Create the folder Modelos 4

---
+++ modelos 4/main.js
@@ -0,0 +1,27 @@
+function criaCalculadora() {
+    return{
+
+        display: document.querySelector('.display')
+
+        inicia() {
+            alert('Oi, iniciei');
+        },
+
+        cliqueBotoes() {
+            document.addEventListener('click', function(e){
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay();
+                }
+            });
+        },
+
+
+
+
+    };
+}
+
+const calculadora = criaCalculadora();
+calculadora.inicia();
