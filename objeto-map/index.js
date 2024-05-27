788dae400023b31d32166c4c22d7912be1eb4a26
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-05-27 09:36:41 -0300

    Comment

--- objeto-map/index.js
+++ objeto-map/index.js
@@ -1,3 +1,5 @@
+//O método map() é uma ferramenta poderosa em JavaScript para transformar arrays de maneira declarativa e imutável.
+
 const pessoas = [       //Array com 3 objetos
    { id: 3, nome: 'Luiz' },
    { id: 2, nome: 'Maria'},
@@ -9,5 +11,7 @@ for (const pessoa of pessoas) {
     const { id } = pessoa;
     novasPessoas.set(id, { ...pessoa});
 }
+
+novasPessoas.delete(3);//para eliminar uma chave
 
 console.log(novasPessoas);
