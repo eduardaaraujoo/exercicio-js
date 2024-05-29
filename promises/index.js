e392e1214ea3c15aa72f650c955e2403275e8b8c
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-05-29 11:22:55 -0300

    Include Promises

--- promises/index.js
+++ promises/index.js
@@ -1,15 +1,34 @@
-function aleatorio(min, max){
+function rand(min, max){
     min *= 1000;
     max *= 1000;
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 function esperaAi(msg, tempo){
+    return new Promise((resolve, reject) =>{
     setTimeout(() => {
-        console.log(msg)
+        resolve(msg);
     }, tempo);
+    });
 }
 
-esperaAi('Frase 1', rand(1, 3));
-esperaAi('Frase 2', rand(1, 3));
-esperaAi('Frase 3', rand(1, 3));
+esperaAi('Conexão com o Banco de Dados', rand(1, 3))
+    .then(resposta => {
+        console.log(resposta);
+        return esperaAi('Buscando dados da BASE', rand(1, 3));
+    })
+    .then(resposta => {
+        console.log(resposta);
+        return esperaAi('Tratando so dados da BASE', rand(1, 3));
+    })
+    .then(resposta => {
+        console.log(resposta);
+    }).then(() => {
+        console.log('Exibe dados na tela');
+    })
+    .catch();
+
+    console.log('Isso daqui será exibido antes de qualquer promise.');
+
+
+ 
