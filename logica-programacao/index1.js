4007f530ad49e30530997b5894d42f86ae5ae222
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-06-03 20:45:36 -0300

    Include the console

--- logica-programacao/index1.js
+++ logica-programacao/index1.js
@@ -1,5 +1,5 @@
 const numeroMaior = (x, y) => {
     return x > y ? x : y;
 }
-const resultado = (numeroMaior(12,9));
-console.log(resultado);
+const resultadoDoNumero = (numeroMaior(12,9));
+console.log(resultadoDoNumero);
